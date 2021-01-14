5ee387d560324f67be78a9be8ce469c0fe35d745
Ksusha <k.shimboretskaya.081221@gmail.com>
2021-01-14 17:57:15 +0300

    Commit2

--- src/test/java/PastebinTest.java
+++ src/test/java/PastebinTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.*;
 import java.util.UUID;
 
@@ -29,30 +30,38 @@ public class PastebinTest {
         System.out.println("The app setup process is completed");
     }
 
-    @Test(priority = 0)
-        public void sendKeys() {
-        String arr[]=UUID.randomUUID().toString().split("-");
+    @Test(priority = 0,description ="Добавление текста и вывод его")
+    public void sendKeys() {
+        String text[]=UUID.randomUUID().toString().split("-");
         WebElement searchField = driver.findElement(By.id("postform-text"));
-        searchField.sendKeys(arr[0]);
+        searchField.sendKeys(text[0]);
         String textAgain = searchField.getAttribute("value");
-        System.out.println("Text in textarea:");
-        System.out.println(textAgain);
+        System.out.println("Text in textarea:" + textAgain);
     }
 
-    @Test(priority = 1)
-    public void clickElement() {
+    @Test(priority = 1, description = "select bash")
+    public void clickSyntaxHighlighting() {
         WebElement selectElement = driver.findElement(By.id("select2-postform-format-container"));
         selectElement.click();
         WebElement options = driver.findElement(By.xpath("//li[contains(text(), 'Bash')]"));
         options.click();
+        String expectedTitle = selectElement.getAttribute("title");
+        String actualTitle = "Bash";
+        Assert.assertEquals(actualTitle, expectedTitle,"No matching");
+    }
+        @Test(priority = 2,description = "select 10 minutes")
+        public void clickPasteExpiration(){
         WebElement pasteExpiration = driver.findElement(By.id("select2-postform-expiration-container"));
         pasteExpiration.click();
         WebElement pasteExp = driver.findElement(By.xpath("//li[contains(text(), '10 Minutes')]"));
         pasteExp.click();
+        String expectedTittle=pasteExpiration.getAttribute("title");
+        String actualTittle="10 Minutes";
+        Assert.assertEquals(actualTittle, expectedTittle,"No matching");
         System.out.println("The click element process is completed");
     }
 
-    @AfterClass
+    @Test(priority=3,description ="Create New Paste")
     public void clickCreateNewPaste() {
         WebElement button = driver.findElement(By.xpath("//button[contains(text(),'Create New Paste')]"));
         button.click();
