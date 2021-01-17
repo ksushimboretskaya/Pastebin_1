9c8736015bd25afb9d64ac13fadbdb452a492063
Ksusha <k.shimboretskaya.081221@gmail.com>
2021-01-17 16:36:44 +0300

    Add new way to set syntax and time, page factory

--- src/test/java/PastebinTest.java
+++ src/test/java/PastebinTest.java
@@ -1,65 +1,54 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
-import java.util.Random;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import org.testng.annotations.DataProvider;
 import org.openqa.selenium.support.PageFactory;
 
 public class PastebinTest {
-    pageObjectModel objPOM;
     private WebDriver driver;
     private static final String URL = "https://pastebin.com";
     private static Logger log = Logger.getLogger(PastebinTest.class.getName());
-    @DataProvider(name = "data-provider")
+    @DataProvider(name = "set-time")
     public Object[][] dataProviderMethod() {
         return new Object[][] { { "10 Minutes" }, { "Never" } };
     }
     @BeforeSuite
-    public void setUp() {
+    public void setWebSite() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
         driver = new ChromeDriver();
-        log.info("The setup process is completed");
-    }
-    @BeforeTest
-    public void profileSetup() {
         driver.manage().window().maximize();
         driver.get(URL);
         log.info("The profile setup process is completed");
     }
-
     @Test(priority =1,description ="Check entered text")
     public void sendTextAndOutput() {
-        objPOM = new pageObjectModel(driver);
-        String symbols = "abcdefghijklmnopqrstuvwxyz"+"1234567890"+"ABCDEFGHIJKLNMOPQRSTUVWXYZ";
-        String random = new Random().ints(10, 0, symbols.length()).mapToObj(symbols::charAt).map(Object::toString).collect(Collectors.joining());
-        objPOM.setText(random);
+        pageObjectModel page = PageFactory.initElements(driver, pageObjectModel.class);
+        page.sendText();
     }
     @Test(priority = 2, description = "Check syntax highlighting ")
     public void clickSyntaxHighlighting() {
-        objPOM = new pageObjectModel(driver);
-        String expectedTitle = "Bash";
-        objPOM.setElementDownList();
-        Assert.assertEquals(objPOM.getActualName(), expectedTitle,"The title of the button doesn't match the expected(bash)");
+        pageObjectModel page = PageFactory.initElements(driver, pageObjectModel.class);
+        page.setElementDownList();
+        page.setSyntax("Bash");
+        Assert.assertEquals(page.getActualName(), "Bash","The title of the button doesn't match the expected(bash)");
     }
-    @Test(priority = 3,description = "Check paste expiration",dataProvider = "data-provider")
+    @Test(priority = 3,description = "Check paste expiration", dataProvider = "set-time")
     public void clickPasteExpiration(String data){
-        objPOM = new pageObjectModel(driver);
-        String time = data;
-        objPOM.setElementPasteExpiration();
-        String expectedTittle="10 Minutes";
-        Assert.assertEquals(objPOM.getActualNamePasteExp(), expectedTittle,"The title of the button doesn't match the expected(10 Minutes)");
+        pageObjectModel page = PageFactory.initElements(driver, pageObjectModel.class);
+        page.setElementPasteExpiration();
+        String expectedTittle = data;
+        page.setTime("10 Minutes");
+        Assert.assertEquals(page.getActualNamePasteExp(), expectedTittle,"The title of the button doesn't match the expected(10 Minutes)");
         log.info("The click element process is completed");
     }
     @Test(priority=4,description ="Create New Paste")
     public void clickCreateNewPaste() {
-        objPOM = new pageObjectModel(driver);
-        objPOM.clickOnTheButtonToCreateNewPaste();
+        pageObjectModel page = PageFactory.initElements(driver, pageObjectModel.class);
+        page.clickOnTheButtonToCreateNewPaste();
         log.info("The click create new paste process is completed");
     }
 }
--- src/test/java/pageObjectModel.java
+++ src/test/java/pageObjectModel.java
@@ -1,42 +1,55 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class pageObjectModel {
     WebDriver driver;
-    private By searchFieldLocator = By.id("postform-text");
-    private By selectElementLocator = By.id("select2-postform-format-container");
-    private By optionsLocator = By.xpath("//li[contains(text(),'Bash')]");
-    private By pasteExpirationLocator = By.id("select2-postform-expiration-container");
-    private By pasteExpLocator = By.xpath("//li[contains(text(), '10 Minutes')]");
-    private By buttonLocator = By.xpath("//button[contains(text(),'Create New Paste')]");
-    public pageObjectModel(WebDriver driver){
-        this.driver=driver;
+    @FindBy(id="postform-text")
+    private WebElement searchFieldLocator;
+    @FindBy(id="select2-postform-format-container")
+    private WebElement selectElementLocator;
+    @FindBy(id ="select2-postform-expiration-container")
+    private WebElement pasteExpirationLocator;
+    @FindBy(xpath = "//button[contains(text(),'Create New Paste')]")
+    private WebElement buttonLocator;
+    public void init(final WebDriver driver){
+        PageFactory.initElements(driver,this);
     }
-    public void setText(String random){
-        driver.findElement(searchFieldLocator).sendKeys(random);
+    public void sendText(){
+        String symbols = "abcdefghijklmnopqrstuvwxyz"+"1234567890"+"ABCDEFGHIJKLNMOPQRSTUVWXYZ";
+        String random = new Random()
+                .ints(10, 0, symbols.length())
+                .mapToObj(symbols::charAt)
+                .map(Object::toString)
+                .collect(Collectors.joining());
+        searchFieldLocator.sendKeys(random);
     }
     public void setElementDownList(){
-        WebElement selectElement = driver.findElement(selectElementLocator);
-        selectElement.click();
-        WebElement options = driver.findElement(optionsLocator);
-        options.click();
-        String actualTitle = driver.findElement(selectElementLocator).getAttribute("title");
+        selectElementLocator.click();
+    }
+    public void setSyntax(String syntax){
+        driver.findElement(By.xpath("//li[contains(text(),'"+syntax+"')]"))
+                .click();
     }
     public String getActualName(){
-        return	driver.findElement(optionsLocator).getAttribute("title");
+        return	selectElementLocator.getAttribute("title");
     }
     public void setElementPasteExpiration(){
-        WebElement pasteExpiration = driver.findElement(pasteExpirationLocator);
-        pasteExpiration.click();
-        WebElement pasteExp = driver.findElement(pasteExpLocator);
-        pasteExp.click();
+        pasteExpirationLocator.click();
+    }
+    public void setTime(String time){
+        driver.findElement(By.xpath("//li[contains(text(),'"+time+"')]"))
+                .click();
     }
     public String getActualNamePasteExp(){
-        return driver.findElement(pasteExpLocator).getAttribute("title");
+        return pasteExpirationLocator.getAttribute("title");
     }
     public void clickOnTheButtonToCreateNewPaste(){
-        WebElement button = driver.findElement(By.xpath("//button[contains(text(),'Create New Paste')]"));
-        button.click();
+        buttonLocator.click();
     }
 }
