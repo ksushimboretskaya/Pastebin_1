7bbc74c183fae85e49d8e455e9fe4c7df369b8c1
Ksusha <k.shimboretskaya.081221@gmail.com>
2021-01-25 19:56:12 +0300

    Change name methods and variables

--- src/test/java/PageResultSearch.java
+++ src/test/java/PageResultSearch.java
@@ -9,47 +9,56 @@ import java.util.stream.Collectors;
 
 public class PageResultSearch {
     WebDriver driver;
-    @FindBy(id="postform-text")
-    private WebElement searchFieldLocatorInputFrame;
-    @FindBy(id="select2-postform-format-container")
-    private WebElement selectElementLocatorSyntax;
-    @FindBy(id ="select2-postform-expiration-container")
-    private WebElement pasteExpirationLocator;
+    @FindBy(id = "postform-text")
+    private WebElement searchFieldInputFrame;
+    @FindBy(id = "select2-postform-format-container")
+    private WebElement selectElementSyntax;
+    @FindBy(id = "select2-postform-expiration-container")
+    private WebElement pasteExpiration;
     @FindBy(xpath = "//button[contains(text(),'Create New Paste')]")
-    private WebElement buttonLocatorCreateNewPaste;
-    public void init(final WebDriver driver){
-        PageFactory.initElements(driver,this);
+    private WebElement buttonCreateNewPaste;
+
+    public void init(final WebDriver driver) {
+        PageFactory.initElements(driver, this);
     }
-    public void sendText(){
-        String symbols = "abcdefghijklmnopqrstuvwxyz"+"1234567890"+"ABCDEFGHIJKLNMOPQRSTUVWXYZ";
+
+    public void sendText() {
+        String symbols = "abcdefghijklmnopqrstuvwxyz" + "1234567890" + "ABCDEFGHIJKLNMOPQRSTUVWXYZ";
         String random = new Random()
                 .ints(10, 0, symbols.length())
                 .mapToObj(symbols::charAt)
                 .map(Object::toString)
                 .collect(Collectors.joining());
-        searchFieldLocatorInputFrame.sendKeys(random);
+        searchFieldInputFrame.sendKeys(random);
     }
-    public void setElementOfSyntaxDownList(){
-        selectElementLocatorSyntax.click();
+
+    public void setElementOfSyntaxDownList() {
+        selectElementSyntax.click();
     }
-    public void setSyntax(String syntax){
-        driver.findElement(By.xpath("//li[contains(text(),'"+syntax+"')]"))
+
+    public void setSyntax(String syntax) {
+        driver.findElement(By.xpath("//li[contains(text(),'" + syntax + "')]"))
                 .click();
     }
-    public String getActualName(){
-        return	selectElementLocatorSyntax.getAttribute("title");
+
+    public String getActualName() {
+        return selectElementSyntax.getAttribute("title");
     }
-    public void setElementPasteExpirationDownList(){
-        pasteExpirationLocator.click();
+
+    public void setElementPasteExpirationDownList() {
+        pasteExpiration.click();
     }
-    public void setTime(String time){
-        driver.findElement(By.xpath("//li[contains(text(),'"+time+"')]"))
+
+    public void setTime(String time) {
+        driver.findElement(By.xpath("//li[contains(text(),'" + time + "')]"))
                 .click();
     }
-    public String getActualNamePasteExp(){
-        return pasteExpirationLocator.getAttribute("title");
+
+    public String getActualNamePasteExp() {
+        return pasteExpiration.getAttribute("title");
     }
-    public void clickOnTheButtonToCreateNewPaste(){
-        buttonLocatorCreateNewPaste.click();
+
+    public void clickOnTheButtonToCreateNewPaste() {
+        buttonCreateNewPaste.click();
     }
 }
--- src/test/java/PastebinTest.java
+++ src/test/java/PastebinTest.java
@@ -2,7 +2,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
 import java.util.logging.Logger;
+
 import org.testng.annotations.DataProvider;
 import org.openqa.selenium.support.PageFactory;
 
@@ -10,10 +12,12 @@ public class PastebinTest {
     private WebDriver driver;
     private static final String URL = "https://pastebin.com";
     private static Logger log = Logger.getLogger(PastebinTest.class.getName());
+
     @DataProvider(name = "set-time")
     public Object[][] dataProviderMethod() {
-        return new Object[][] { { "10 Minutes" }, { "Never" } };
+        return new Object[][]{{"10 Minutes"}, {"Never"}};
     }
+
     @BeforeTest
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
@@ -22,29 +26,37 @@ public class PastebinTest {
         driver.get(URL);
         log.info("The profile setup process is completed");
     }
-    @Test(priority =1,description ="Checking text input")
+
+    @BeforeGroups("selectListItem")
     public void sendText() {
         PageResultSearch pageSearch = PageFactory.initElements(driver, PageResultSearch.class);
         pageSearch.sendText();
     }
-    @Test(priority = 2, description = "Checking syntax highlighting input")
+
+    @Test(priority = 1, description = "Checking syntax highlighting input", groups = "selectListItem")
     public void syntaxValidation() {
         PageResultSearch page = PageFactory.initElements(driver, PageResultSearch.class);
         page.setElementOfSyntaxDownList();
-        page.setSyntax("Bash");
-        Assert.assertEquals(page.getActualName(), "Bash","The title of the button doesn't match the expected(bash)");
+        String selectItemSyntax = "Bash";
+        String selectListItemSintax = String.format("%s", selectItemSyntax);
+        page.setSyntax(selectItemSyntax);
+        Assert.assertEquals(page.getActualName(), "Bash", "Selected list item doesn't match the expected:" + selectListItemSintax + "");
     }
-    @Test(priority = 3,description = "Checking paste expiration input ", dataProvider = "set-time")
-    public void PasteExpirationValidation(String data){
+
+    @Test(priority = 2, description = "Checking paste expiration input ", dataProvider = "set-time", groups = "selectListItem")
+    public void pasteExpirationValidation(String data) {
         PageResultSearch page = PageFactory.initElements(driver, PageResultSearch.class);
         page.setElementPasteExpirationDownList();
         String expectedTittle = data;
-        page.setTime("10 Minutes");
-        Assert.assertEquals(page.getActualNamePasteExp(), expectedTittle,"The title of the button doesn't match the expected(10 Minutes)");
+        String selectItemPasteExpiration = "10 Minutes";
+        String selectItemPasteExp = String.format("%s", selectItemPasteExpiration);
+        page.setTime(selectItemPasteExpiration);
+        Assert.assertEquals(page.getActualNamePasteExp(), expectedTittle, "Selected list item doesn't match the expected:" + selectItemPasteExp + "");
         log.info("The click element process is completed");
     }
-    @Test(priority=4,description ="Create New Paste")
-    public void clickToCreateNewPaste() {
+
+    @Test(priority = 3, description = "Create New Paste", groups = "selectListItem")
+    public void checkCreateNewPasteButton() {
         PageResultSearch page = PageFactory.initElements(driver, PageResultSearch.class);
         page.clickOnTheButtonToCreateNewPaste();
         log.info("The click create new paste process is completed");
